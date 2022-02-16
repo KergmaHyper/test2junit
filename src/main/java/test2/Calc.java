e2d314136cc3278dc06901a7584343b865011293
kergma@gmail.com <kergma@gmail.com>
2022-02-16 16:20:42 +0200

    first webdriver run

--- src/main/java/test2/Calc.java
+++ src/main/java/test2/Calc.java
@@ -1,5 +1,16 @@
 package test2;
 
+import java.util.Scanner;
+
+import com.google.common.math.Quantiles.Scale;
+
+//import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.firefox.FirefoxOptions;
+
 public class Calc {
 
     public static int add(int x, int y) {
@@ -16,6 +27,23 @@ public class Calc {
 
     public static int sub(int x, int y) {
         return x - y;
+    }
+
+    public static void main(String[] args) {
+
+        ChromeOptions options = new ChromeOptions();
+        var driver = new ChromeDriver(options);
+        driver.get("https://sd.ua");
+        String str = driver.getTitle();
+        System.out.printf("\n\n%str\n\n", str);
+        driver.quit();
+
+        // FirefoxOptions options1 = new FirefoxOptions();
+        // var driver1 = new FirefoxDriver(options1);
+        // driver1.get("https://sd.ua");
+        // String str = driver1.getTitle();
+        // System.out.printf("\n\n%str\n\n", str);
+        // driver1.quit();
     }
 
 }
